8a01b0e18dfccc06280c09c8f6633c8b3177c3a0
p.sz1 <p.sz1@op.pl>
2020-03-24 20:56:51 +0100

    English version

--- main.cpp
+++ main.cpp
@@ -18,13 +18,15 @@ using namespace std;
 
 
 //Wybór wielkości planszy
-static const int gameBoardSize = 12;
+static const int gameBoardSize = 3;
 
 static const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-char poprzedniRuch[gameBoardSize][gameBoardSize];
+char previousMove[gameBoardSize][gameBoardSize];
 
-void skopiujWartosci(char t1[gameBoardSize][gameBoardSize], char t2[gameBoardSize][gameBoardSize]){
+
+
+void copyTab(char t1[gameBoardSize][gameBoardSize], char t2[gameBoardSize][gameBoardSize]){
     for (int i = 0; i < gameBoardSize; i++){
         for (int j = 0; j < gameBoardSize; j++){
             t1[i][j] = t2[i][j];
@@ -32,7 +34,7 @@ void skopiujWartosci(char t1[gameBoardSize][gameBoardSize], char t2[gameBoardSiz
     }
 }
 
-void rysujSkrajnyRzad(){
+void drawExtremeRow(){
     cout<<" ";
     for (int i = 0; i < gameBoardSize*2 + 5; ++i) {
         cout<<"-";
@@ -40,10 +42,10 @@ void rysujSkrajnyRzad(){
     cout<< " "<<endl;
 }
 
-void tabela(char t[gameBoardSize][gameBoardSize]){
+void drawTable(char t[gameBoardSize][gameBoardSize]){
 
     //pierwszy rząd poziomy
-    rysujSkrajnyRzad();
+    drawExtremeRow();
 
     //rząd z nazwami kolumn
     cout<<"|   ";
@@ -62,12 +64,12 @@ void tabela(char t[gameBoardSize][gameBoardSize]){
     }
 
     //ostatni rząd poziomy
-    rysujSkrajnyRzad();
+    drawExtremeRow();
 }
 
-bool pobierz(char t[gameBoardSize][gameBoardSize], int & kto, char znak1, char znak2){
+bool getTab(char t[gameBoardSize][gameBoardSize], int & who, char znak1, char znak2){
     char poz[2];
-    if(!(kto%2))
+    if(!(who % 2))
         cout<<"Kolko, ";
     else
         cout<<"Krzyzyk, ";
@@ -77,17 +79,17 @@ bool pobierz(char t[gameBoardSize][gameBoardSize], int & kto, char znak1, char z
 
         //cofanie ruchu
         if(poz[0]=='q') {
-            skopiujWartosci(t, poprzedniRuch);
+            copyTab(t, previousMove);
             return true;
         }
-    }while((poz[0]<'a' || poz[0]>(char)alphabet[gameBoardSize]) || (poz[1]<'1' || poz[1]>(char)gameBoardSize));
+    }while((poz[0]<'a' || poz[0]>(char)alphabet[gameBoardSize]) || (poz[1]<'1' || poz[1]>(char)alphabet[gameBoardSize]));
 
     if(t[poz[1]-'1'][poz[0]-'a']!=' '){
         cout<< endl <<"Pole "<<poz<<" bylo juz uzupelnione."<<endl;
-        return pobierz(t, kto, znak1, znak2);
+        return getTab(t, who, znak1, znak2);
     }
 
-    if(!(kto++%2))
+    if(!(who++ % 2))
         t[poz[1]-'1'][poz[0]-'a']=znak1;
     else
         t[poz[1]-'1'][poz[0]-'a']=znak2;
@@ -95,7 +97,7 @@ bool pobierz(char t[gameBoardSize][gameBoardSize], int & kto, char znak1, char z
     return true;
 }
 
-bool rozwiazanie(char t[gameBoardSize][gameBoardSize]){
+bool solution(char t[gameBoardSize][gameBoardSize]){
     int bz=0;
     bool x, y;
     bool p1=true, p2=true;
@@ -146,84 +148,84 @@ bool rozwiazanie(char t[gameBoardSize][gameBoardSize]){
     return false;
 }
 
-bool postawWRogu(char t[gameBoardSize][gameBoardSize]){
-    bool ruchZrobiony = false;
+bool putInCorner(char t[gameBoardSize][gameBoardSize]){
+    bool moveMade = false;
     if(t[0][0] == 'O'){
         if(t[0][2] == ' ' && t[0][1] == ' '){
             t[0][2] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }else if(t[2][0] == ' ' && t[1][0] == ' '){
             t[2][0] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }
     }else if(t[0][2] == 'O'){
         if(t[0][0] == ' ' && t[0][1] == ' '){
             t[0][0] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }else if(t[2][2] == ' ' && t[1][2] == ' '){
             t[2][2] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }
     }else if(t[2][0] == 'O'){
         if(t[0][0] == ' ' && t[1][0] == ' '){
             t[0][0] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }else if(t[2][2] == ' ' && t[2][1] == ' '){
             t[2][2] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }
     }else if(t[2][2] == 'O'){
         if(t[0][2] == ' ' && t[1][2] == ' '){
             t[0][2] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }else if(t[2][0] == ' ' && t[2][1] == ' '){
             t[2][0] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }
     }else if(t[0][0] == 'X' && t[2][0] == 'X' && t[0][2] == 'X' && t[2][2] == 'X'){
-        ruchZrobiony = false;
+        moveMade = false;
     }else{
-        while(!ruchZrobiony){
+        while(!moveMade){
             int random = rand() % 4 + 1;
             if(random == 1 && t[0][0] == ' '){
                 t[0][0] = 'O';
-                ruchZrobiony = true;
+                moveMade = true;
             }else if(random == 2 && t[0][2] == ' '){
                 t[0][gameBoardSize - 1] = 'O';
-                ruchZrobiony = true;
+                moveMade = true;
             }else if(random == 3 && t[2][0] == ' '){
                 t[2][0] = 'O';
-                ruchZrobiony = true;
+                moveMade = true;
             }else if(random == 4 && t[2][2] == ' '){
                 t[2][2] = 'O';
-                ruchZrobiony = true;
+                moveMade = true;
             }
         }
     }
-    return ruchZrobiony;
+    return moveMade;
 }
 
-bool koncz(char t[gameBoardSize][gameBoardSize], char znak){
+bool tryToFinish(char t[gameBoardSize][gameBoardSize], char character){
     int vertical = 0, horizontal = 0, diagonal = 0;
     int blankVertical = 0, blankHorizontal = 0, blankDiagonal = 0;
     int blankVerticalPoz = 0, blankHorizontalPoz = 0, blankDiagonalPoz = 0;
     for (int i = 0; i < gameBoardSize; ++i) {
         for (int j = 0; j < gameBoardSize; ++j) {
-            if(t[i][j] == znak){
+            if(t[i][j] == character){
                 horizontal++;
             }else if(t[i][j] == ' '){
                 blankHorizontal++;
                 blankHorizontalPoz = j;
             }
 
-            if(t[j][i] == znak){
+            if(t[j][i] == character){
                 vertical++;
-            }else if(t[j][i] == znak){
+            }else if(t[j][i] == character){
                 blankVertical++;
                 blankVerticalPoz = j;
             }
         }
-        if(t[i][i] == znak){
+        if(t[i][i] == character){
             diagonal++;
         }else if(t[i][i] == ' ') {
             blankDiagonal++;
@@ -252,26 +254,26 @@ bool koncz(char t[gameBoardSize][gameBoardSize], char znak){
 }
 
 void randomMoves(char t[gameBoardSize][gameBoardSize]) {
-    bool ruchZrobiony = false;
-    while(!ruchZrobiony){
+    bool moveMade = false;
+    while(!moveMade){
         int randomX = rand() % gameBoardSize;
         int randomY = rand() % gameBoardSize;
 
         if(t[randomY][randomX] == ' '){
             t[randomY][randomX] = 'O';
-            ruchZrobiony = true;
+            moveMade = true;
         }
     }
 
 }
 
-void AdvancedAI(char t[gameBoardSize][gameBoardSize], int ruch){
-        if(ruch < 3){
-            postawWRogu(t);
+void AdvancedAI(char t[gameBoardSize][gameBoardSize], int move){
+        if(move < 3){
+            putInCorner(t);
         }else{
-            if(!koncz(t, 'O'))
-                if(!koncz(t, 'X'))
-                    if(!postawWRogu(t))
+            if(!tryToFinish(t, 'O'))
+                if(!tryToFinish(t, 'X'))
+                    if(!putInCorner(t))
                         randomMoves(t);
         }
 }
@@ -281,53 +283,53 @@ int main(){
 
     char tab[gameBoardSize][gameBoardSize];
     for(int i=0; i<gameBoardSize; i++) for(int j=0; j<gameBoardSize; tab[i][j++]=' ');
-    skopiujWartosci(poprzedniRuch, tab);
-    int kto = 0;
-    int ruch = 1;
-    bool koniec=false;
+    copyTab(previousMove, tab);
+    int who = 0;
+    int move = 1;
+    bool end = false;
 
-    int poziomTrudnosci = 0;
+    int difficultyLevel = 0;
 
     cout << "Wybierz poziom trudnosci: ";
-    cin >> poziomTrudnosci;
+    cin >> difficultyLevel;
 
     bool AI = true;
 
     int temp = 0;
     int i = 0;
-    while(!koniec) {
+    while(!end) {
         if (i % 2 == 0) {
-            skopiujWartosci(poprzedniRuch, tab);
-            if (poziomTrudnosci == 0) {
+            copyTab(previousMove, tab);
+            if (difficultyLevel == 0) {
                 randomMoves(tab);
-            } else if (poziomTrudnosci == 1) {
+            } else if (difficultyLevel == 1) {
                 if(temp%2 == 0) {
                     randomMoves(tab);
                 }else {
-                    AdvancedAI(tab, ruch);
+                    AdvancedAI(tab, move);
                 }
                 temp++;
-            } else if (poziomTrudnosci == 2) {
-                AdvancedAI(tab, ruch);
+            } else if (difficultyLevel == 2) {
+                AdvancedAI(tab, move);
             }
-        ruch++;
-        kto += 1;
+        move++;
+            who += 1;
 
-        tabela(tab);
-        koniec = rozwiazanie(tab);
+            drawTable(tab);
+            end = solution(tab);
         }
         if(i%2 == 1){
             //Mozliwość wyboru znaków do gry
-            pobierz(tab, kto, 'O', 'X');
+            getTab(tab, who, 'O', 'X');
             if(AI){
-                tabela(tab);
+                drawTable(tab);
                 cout << "Ruch komputera (nacisnij dowolny klawisz aby kontynuowac)";
                 getch();
             }
-            koniec=rozwiazanie(tab);
+            end= solution(tab);
         }
         i++;
     }
-    tabela(tab);
+    drawTable(tab);
     return 0;
 }
